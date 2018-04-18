58c50f7d37d0210547450346183646109127ad1c
diegopf <diegopascual.f@gmail.com>
2018-04-18 23:42:02 +0200

    feat(calculateTotal): add function to calculate total distance

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,29 @@
-const functions = require('firebase-functions');
+const functions = require("firebase-functions");
+const admin = require("firebase-admin");
+admin.initializeApp({
+  credential: admin.credential.applicationDefault()
+});
+const firestore = admin.firestore();
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+const tripsDocuments = "/history/trips/{userId}/{tripId}";
+
+exports.calculateTotal = functions.firestore
+  .document(tripsDocuments)
+  .onCreate(ev => {
+    const document = ev.data();
+    return firestore
+      .collection(`/history/trips/${document.userId}`)
+      .get()
+      .then(data => {
+        const docs = [];
+        data.forEach(d => docs.push(d.data()));
+        const total = docs.map(d => d.distance).reduce((a, b) => (a += b));
+        return firestore
+          .collection("/stats")
+          .doc(document.userId)
+          .set({ total });
+      })
+      .catch(error => {
+        console.error(error);
+      });
+  });
